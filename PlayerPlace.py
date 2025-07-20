638cc249ca33fbe5c6c3328553bf109c5f4256ef
Ovladychenko <ovladychenko@mail.ru>
2025-07-20 22:37:59 +0300

    check answer

--- PlayerPlace.py
+++ PlayerPlace.py
@@ -106,14 +106,30 @@ class PlayerPlace:
             if not ',' in answer_item:
                 return False
             answer_array = answer_item.split(',')
-            #получаем карту на столе
-            cart_dict = self.cards_on_table_list[int(answer_array[0])-1]
+            # получаем карту на столе
+            cart_dict = self.cards_on_table_list[int(answer_array[0]) - 1]
             cart_table = cart_dict.get('cart')
-            #получаем карту игрока
-            cart_user = self.main_player.cards[int(answer_array[1])-1]
-            if cart_user.priority > cart_table.priority and cart_user.suit == cart_table.suit:
-                print('Бьет карту')
+            # получаем карту игрока
+            cart_user = self.main_player.cards[int(answer_array[1]) - 1]
+            if (cart_user.priority > cart_table.priority and cart_user.suit == cart_table.suit) or (
+                    cart_user.is_trump and not cart_table.is_trump) or (
+                    (cart_user.is_trump and cart_table.is_trump) and (cart_user.priority > cart_table.priority)
+            ):
+                cart_dict['cart_link'] = cart_user
+                self.main_player.give_cart(cart_user)
+                pass
+            else:
+                print('Карта ' + cart_user.name + ' не может бить карту ' + cart_table.name)
+
         return is_error
+
+    # Проверка биты ли все карты на столе
+    def is_card_bits(self):
+        index = 0
+        for cart_item in self.cards_on_table_list:
+            if cart_item.get('cart_link') is not None:
+                index += 1
+        return  len(self.cards_on_table_list) == index
 
     def start_player_game(self):
         first_step = True
@@ -149,6 +165,8 @@ class PlayerPlace:
                 return 0
             else:
                 result = self.processing_answer(answer)
-
+            if self.is_card_bits():
+                print('Отбой карт, ход переходит следующему игроку!')
+                return 0
             first_step = False
             self.print_border()
